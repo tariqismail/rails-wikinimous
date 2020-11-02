6ef703bfd1e9f4b972dfb66ddb86f17ee54822d7
Tariq Ismail <tariqismail@gmail.com>
2020-11-02 15:47:48 +0400

    setup bootstrap and simple_form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Seeding articles"
+
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Quote.yoda
+  )
+end
+
+puts "#{Article.count} articles created."
