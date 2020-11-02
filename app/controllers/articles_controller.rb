6adced0b7e68bde2eab1d28c27e2f191df0cb0ea
Tariq Ismail <tariqismail@gmail.com>
2020-11-02 16:29:10 +0400

    show and new/create actions complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,21 +4,32 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 
 end
